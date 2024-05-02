e2d5f107d4c7d0567e489afd3b4cb48361cddcac
Felipe Lambak <Lambakfelipe@gmail.com>
2024-05-02 17:35:13 -0300

    Update EstacMet.ino

--- EstacMet/EstacMet.ino
+++ EstacMet/EstacMet.ino
@@ -1,10 +1,15 @@
+/*Bibliotecas e variáveis para utilização do DHT22 */
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
+DHT dht(2, DHT22);
+float umid, temp;
+
 
 void setup() { 
+  dht.begin(); /*Inicia a comunicação da biblioteca DHT*/
 
 }
 void loop(){
-    umid = dht.readHumidity();
+  umid = dht.readHumidity();
   temp = dht.readTemperature();
 }
