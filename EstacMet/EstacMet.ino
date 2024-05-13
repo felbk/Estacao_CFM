b7d6158eb3cd36e6cd6c57f39d878656984bb2b9
Felipe Lambak <Lambakfelipe@gmail.com>
2024-05-13 14:05:53 -0300

    Tentativa de implementação botão

--- EstacMet/EstacMet.ino
+++ EstacMet/EstacMet.ino
@@ -23,10 +23,11 @@ float temp ;
 float Ad_temp;
 
 //Incertezas
-float IncLM35 = 0.54; // incerteza padrão de medição do LM35 (°C)
+float IncLM35 = 0.50; // incerteza padrão de medição do LM35 (°C)
 float IncDHT22 = 2; //incerteza padrão de medição do DHT22 (%)
 float IncBMP280Press = 0.12; //incerteza padrão de medição de pressão do BMP (hPa)
 float IncBMP280Alt = 1; //incerteza padrão de medição de altitude do BMP (m)
+
 //Variáveis para manipulação do BMP280
 Adafruit_BMP280 bmp; //OBJETO DO TIPO Adafruit_BMP280 (I2C)
 float pressao,alt; // declaracao de variaveis
@@ -35,8 +36,12 @@ float pressao,alt; // declaracao de variaveis
 int bin;
 float Vout, RLDR, lum;
 
+// Variável para modo do botão 
+int Bt = 0;
+
 #define  LM A0
 #define LDR A1
+#define Botao 2
 
 
 void setup() { 
@@ -45,6 +50,7 @@ void setup() {
   analogReference(INTERNAL); // Conversao A/D com fundo de escala de 1.1V para o LM35
   pinMode(LM,INPUT); // Inicia o pino do LM35 como entrada
   pinMode(LDR,INPUT); // Inicia o pino do LDR como entrada
+  pinMode(Botao , INPUT_PULLUP); //Inicia o pino do botão utilizando o pull up interno do arduino
   
 
  //Inicia LCD
@@ -83,9 +89,9 @@ void setup() {
   lcd.clear();
 }
 void loop(){
-  // Inicia 
  
-
+ 
+ //Inicia coleta e conversão de dados:
   umid = dht.readHumidity(); // salva umidade sem ajuste
   Ad_umid = 1.2*umid - 11.82;
 
@@ -105,79 +111,105 @@ void loop(){
 //LDR
 bin = analogRead(LDR);
 Vout = bin*1.1/1024.0;
-RLDR = (3.3-Vout)/Vout * 49820
-lum = 
+RLDR = (3.3-Vout)/Vout * 49820;
+lum = pow(10,-1.2*log10(RLDR+21451.94));
 
 
-//EXIBE INFORMAÇÕES
-// Exibe pressão
-  lcd.setCursor(0,0);  // posiciona o cursor
-  lcd.print("Pressao:");  // Escreve no LCD
-  lcd.setCursor(8,0);  // posiciona o cursor
-  lcd.print(pressao);  // Escreve no LCD
-  lcd.setCursor(13,0);  // posiciona o cursor
-  lcd.print("hPa");  // Escreve no LCD
-  //Exibe a incerteza da pressão
-  lcd.setCursor(0,1);  // posiciona o cursor
-  lcd.print("Incert:+-");  // Escreve no LCD
-  lcd.setCursor(10,1);  // posiciona o cursor
-  lcd.print(IncBMP280Press,2);  // Escreve no LCD
-  lcd.setCursor(13,1);  // posiciona o cursor
-  lcd.print("hPa");  // Escreve no LCD
-  delay(3000);
-  lcd.clear();
+  // Analisa modo do botão
+  if (digitalRead(Botao) == 0) {
+    Bt = Bt +1;  // Avança um modo
+    delay(1400);
+  }
+  if (Bt == 6){ Bt = 0;} //volta para o primeiro modo
 
-// Exibe altitude
-   lcd.setCursor(0,0);  // posiciona o cursor
-  lcd.print("Altitude:");  // Escreve no LCD
-  lcd.setCursor(10,0);  // posiciona o cursor
-  lcd.print(alt);  // Escreve no LCD
-  lcd.setCursor(15,0);  // posiciona o cursor
-  lcd.print("m");  // Escreve no LCD
- //Exibe a incerteza da altitude
-  lcd.setCursor(0,1);  // posiciona o cursor
-  lcd.print("Incert:+-");  // Escreve no LCD
-  lcd.setCursor(10,1);  // posiciona o cursor
-  lcd.print(IncBMP280Alt,2);  // Escreve no LCD
-  lcd.setCursor(15,1);  // posiciona o cursor
-  lcd.print("m");  // Escreve no LCD
-  delay(3000);
-  lcd.clear();
+  // se o modo for 0 ele exibe todas informações, os demais modos representam respectivamente :
+  // Temperatura, Umidade, Pressão, Altitude, Iluminância
 
-  //Exibe umidade
-     lcd.setCursor(0,0);  // posiciona o cursor
-  lcd.print("Umidade:");  // Escreve no LCD
-  lcd.setCursor(9,0);  // posiciona o cursor
-  lcd.print(Ad_umid);  // Escreve no LCD
-  lcd.setCursor(15,0);  // posiciona o cursor
-  lcd.print("%");  // Escreve no LCD
+  //EXIBE INFORMAÇÕES
 
-   //Exibe a incerteza da umidade
-  lcd.setCursor(0,1);  // posiciona o cursor
-  lcd.print("Incert:+-");  // Escreve no LCD
-  lcd.setCursor(10,1);  // posiciona o cursor
-  lcd.print(IncDHT22,2);  // Escreve no LCD
-  lcd.setCursor(15,1);  // posiciona o cursor
-  lcd.print("%");  // Escreve no LCD
-  delay(3000);
-  lcd.clear();
+  if (Bt == 0 || Bt == 1) {
+    // Exibe temperatura
+    lcd.setCursor(0,0);  // posiciona o cursor
+    lcd.print("Temp:");  // Escreve no LCD
+    lcd.setCursor(6,0);  // posiciona o cursor
+    lcd.print(Ad_temp);  // Escreve no LCD
+    lcd.setCursor(14,0);  // posiciona o cursor
+    lcd.print("oC");  // Escreve no LCD
+    //Exibe a incerteza da temperatura
+    lcd.setCursor(0,1);  // posiciona o cursor
+    lcd.print("Incert:+-");  // Escreve no LCD
+    lcd.setCursor(10,1);  // posiciona o cursor
+    lcd.print(IncLM35,2);  // Escreve no LCD
+    lcd.setCursor(14,1);  // posiciona o cursor
+    lcd.print("oC");  // Escreve no LCD
+    delay(3000);
+    lcd.clear(); 
+    }
+  if (Bt == 0 || Bt == 2) {
+    //Exibe umidade
+      lcd.setCursor(0,0);  // posiciona o cursor
+    lcd.print("Umidade:");  // Escreve no LCD
+    lcd.setCursor(9,0);  // posiciona o cursor
+    lcd.print(Ad_umid);  // Escreve no LCD
+    lcd.setCursor(15,0);  // posiciona o cursor
+    lcd.print("%");  // Escreve no LCD
 
-  // Exibe temperatura
-  lcd.setCursor(0,0);  // posiciona o cursor
-  lcd.print("Temp:");  // Escreve no LCD
-  lcd.setCursor(6,0);  // posiciona o cursor
-  lcd.print(Ad_temp);  // Escreve no LCD
-  lcd.setCursor(14,0);  // posiciona o cursor
-  lcd.print("oC");  // Escreve no LCD
-  //Exibe a incerteza da temperatura
-  lcd.setCursor(0,1);  // posiciona o cursor
-  lcd.print("Incert:+-");  // Escreve no LCD
-  lcd.setCursor(10,1);  // posiciona o cursor
-  lcd.print(IncLM35,2);  // Escreve no LCD
-  lcd.setCursor(14,1);  // posiciona o cursor
-  lcd.print("oC");  // Escreve no LCD
-  delay(3000);
-  lcd.clear();
+    //Exibe a incerteza da umidade
+    lcd.setCursor(0,1);  // posiciona o cursor
+    lcd.print("Incert:+-");  // Escreve no LCD
+    lcd.setCursor(10,1);  // posiciona o cursor
+    lcd.print(IncDHT22,2);  // Escreve no LCD
+    lcd.setCursor(15,1);  // posiciona o cursor
+    lcd.print("%");  // Escreve no LCD
+    delay(3000);
+    lcd.clear();
+  }
+  if (Bt == 0 || Bt == 3) {
+  // Exibe pressão
+    lcd.setCursor(0,0);  // posiciona o cursor
+    lcd.print("Pressao:");  // Escreve no LCD
+    lcd.setCursor(8,0);  // posiciona o cursor
+    lcd.print(pressao);  // Escreve no LCD
+    lcd.setCursor(13,0);  // posiciona o cursor
+    lcd.print("hPa");  // Escreve no LCD
+    //Exibe a incerteza da pressão
+    lcd.setCursor(0,1);  // posiciona o cursor
+    lcd.print("Incert:+-");  // Escreve no LCD
+    lcd.setCursor(10,1);  // posiciona o cursor
+    lcd.print(IncBMP280Press,2);  // Escreve no LCD
+    lcd.setCursor(13,1);  // posiciona o cursor
+    lcd.print("hPa");  // Escreve no LCD
+    delay(3000);
+    lcd.clear();
+  }
+
+  if (Bt == 0 || Bt == 4) {
+  // Exibe altitude
+    lcd.setCursor(0,0);  // posiciona o cursor
+    lcd.print("Altitude:");  // Escreve no LCD
+    lcd.setCursor(10,0);  // posiciona o cursor
+    lcd.print(alt);  // Escreve no LCD
+    lcd.setCursor(15,0);  // posiciona o cursor
+    lcd.print("m");  // Escreve no LCD
+  //Exibe a incerteza da altitude
+    lcd.setCursor(0,1);  // posiciona o cursor
+    lcd.print("Incert:+-");  // Escreve no LCD
+    lcd.setCursor(10,1);  // posiciona o cursor
+    lcd.print(IncBMP280Alt,2);  // Escreve no LCD
+    lcd.setCursor(15,1);  // posiciona o cursor
+    lcd.print("m");  // Escreve no LCD
+    delay(3000);
+    lcd.clear();
+  }
+  if (Bt == 0 || Bt == 5) {
+  // Exibe Iluminância
+
+
+
+
+
+
+  }
 
 
 }
